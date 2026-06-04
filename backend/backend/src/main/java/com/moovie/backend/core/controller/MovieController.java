d8cf2ac1e9eb53733034db8126bd28f1b212b8c8
Bruno Bacelar Abe <abe2602@gmail.com>
2026-06-04 20:35:21 -0300

    [chore] Create SpringBoot structure

---
+++ backend/backend/src/main/java/com/moovie/backend/core/controller/MovieController.java
@@ -0,0 +1,4 @@
+package com.moovie.backend.core.controller;
+
+public class MovieController {
+}
---
+++ backend/backend/src/main/java/com/moovie/backend/domain/model/Movie.java
@@ -0,0 +1,7 @@
+package com.moovie.backend.domain.model;
+
+public record Movie(
+        int id,
+        String name
+) {
+}
